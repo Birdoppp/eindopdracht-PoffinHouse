94599a41bdaef69e6762d9cd0e9745e98e3b4556
Bridoppp <tini.pham99@gmail.com>
2024-04-22 19:00:15 +0200

    Images added for Preferences

--- src/components/BerryToggle/BerryToggle.jsx
+++ src/components/BerryToggle/BerryToggle.jsx
@@ -2,12 +2,7 @@ import React from 'react';
 import './BerryToggle.css'
 import {ChevronLeftIcon, ChevronRightIcon} from "@radix-ui/react-icons";
 
-// eslint-disable-next-line react-refresh/only-export-components,react/prop-types,react/display-name
 let BerryToggle = ({handleClick, isOpen}) => {
-
-    // const [isOpen, toggleIsOpen] = useState(true)
-    // const handleClickIsOpen = () => toggleIsOpen(prev=>!prev)
-
     return (
         <>
             <div className="pink-toggle">
